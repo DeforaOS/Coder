c9ce8d657490ac17fe29cb1f70ec715b983b74f3
Pierre Pronchery <khorben@defora.org>
2015-06-18 17:46:32 -0400

    Initial import of the DeforaOS Coder Debugger

---
+++ tools/debugger-main.c
@@ -0,0 +1,106 @@
+/* $Id$ */
+/* Copyright (c) 2015 Pierre Pronchery <khorben@defora.org> */
+/* Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the authors nor the names of the contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY ITS AUTHORS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE. */
+
+
+
+#include <unistd.h>
+#include <stdio.h>
+#include <locale.h>
+#include <libintl.h>
+#include <gtk/gtk.h>
+#include <System.h>
+#include "debugger.h"
+#include "../config.h"
+#define _(string) gettext(string)
+
+/* constants */
+#ifndef PROGNAME
+# define PROGNAME	"debugger"
+#endif
+#ifndef PREFIX
+# define PREFIX		"/usr/local"
+#endif
+#ifndef DATADIR
+# define DATADIR	PREFIX "/share"
+#endif
+#ifndef LOCALEDIR
+# define LOCALEDIR	DATADIR "/locale"
+#endif
+
+
+/* private */
+/* prototypes */
+static int _error(char const * message, int ret);
+static int _usage(void);
+
+
+/* functions */
+/* error */
+static int _error(char const * message, int ret)
+{
+	fputs(PROGNAME ": ", stderr);
+	perror(message);
+	return ret;
+}
+
+
+/* usage */
+static int _usage(void)
+{
+	fprintf(stderr, _("Usage: %s\n"), PROGNAME);
+	return 1;
+}
+
+
+/* public */
+/* functions */
+/* main */
+int main(int argc, char * argv[])
+{
+	int o;
+	Debugger * debugger;
+
+	if(setlocale(LC_ALL, "") == NULL)
+		_error("setlocale", 1);
+	bindtextdomain(PACKAGE, LOCALEDIR);
+	textdomain(PACKAGE);
+	gtk_init(&argc, &argv);
+	while((o = getopt(argc, argv, "")) != -1)
+		switch(o)
+		{
+			default:
+				return _usage();
+		}
+	if(optind != argc)
+		return _usage();
+	if((debugger = debugger_new()) == NULL)
+	{
+		error_print(PROGNAME);
+		return 2;
+	}
+	gtk_main();
+	debugger_delete(debugger);
+	return 0;
+}
---
+++ tools/debugger.c
@@ -0,0 +1,122 @@
+/* $Id$ */
+static char const _debugger_copyright[] =
+"Copyright © 2015 Pierre Pronchery <khorben@defora.org>";
+static char const _debugger_license[] =
+"Redistribution and use in source and binary forms, with or without\n"
+"modification, are permitted provided that the following conditions\n"
+"are met:\n"
+"1. Redistributions of source code must retain the above copyright\n"
+"   notice, this list of conditions and the following disclaimer.\n"
+"2. Redistributions in binary form must reproduce the above copyright\n"
+"   notice, this list of conditions and the following disclaimer in the\n"
+"   documentation and/or other materials provided with the distribution.\n"
+"3. Neither the name of the authors nor the names of the contributors\n"
+"   may be used to endorse or promote products derived from this software\n"
+"   without specific prior written permission.\n"
+"THIS SOFTWARE IS PROVIDED BY ITS AUTHORS AND CONTRIBUTORS ``AS IS'' AND\n"
+"ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\n"
+"IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE\n"
+"ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE\n"
+"FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL\n"
+"DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS\n"
+"OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)\n"
+"HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT\n"
+"LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY\n"
+"OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF\n"
+"SUCH DAMAGE.";
+
+
+
+#include <libintl.h>
+#include <gtk/gtk.h>
+#include <System.h>
+#include "debugger.h"
+#define _(string) gettext(string)
+
+
+/* Debugger */
+/* private */
+/* types */
+struct _Debugger
+{
+	/* widgets */
+	GtkWidget * window;
+	/* toolbar */
+	/* registers */
+	GtkWidget * reg_view;
+};
+
+
+/* prototypes */
+/* callbacks */
+static void _debugger_on_close(gpointer data);
+static gboolean _debugger_on_closex(gpointer data);
+
+
+/* public */
+/* functions */
+/* debugger_new */
+Debugger * debugger_new(void)
+{
+	Debugger * debugger;
+	GtkWidget * vbox;
+	GtkWidget * paned;
+	GtkWidget * widget;
+	GtkListStore * store;
+
+	if((debugger = object_new(sizeof(*debugger))) == NULL)
+		return NULL;
+	debugger->window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(debugger->window), _("Debugger"));
+	g_signal_connect_swapped(debugger->window, "delete-event", G_CALLBACK(
+				_debugger_on_closex), debugger);
+	vbox = gtk_vbox_new(FALSE, 0);
+	/* menubar */
+	/* toolbar */
+	/* view */
+	paned = gtk_hpaned_new();
+	/* registers */
+	widget = gtk_scrolled_window_new(NULL, NULL);
+	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(widget),
+			GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+	store = gtk_list_store_new(2,
+			G_TYPE_STRING,	/* name */
+			G_TYPE_STRING);	/* value */
+	debugger->reg_view = gtk_tree_view_new_with_model(
+			GTK_TREE_MODEL(store));
+	gtk_container_add(GTK_CONTAINER(widget), debugger->reg_view);
+	gtk_paned_add1(GTK_PANED(paned), widget);
+	gtk_box_pack_start(GTK_BOX(vbox), paned, TRUE, TRUE, 0);
+	gtk_container_add(GTK_CONTAINER(debugger->window), vbox);
+	gtk_widget_show_all(debugger->window);
+	return debugger;
+}
+
+
+/* debugger_delete */
+void debugger_delete(Debugger * debugger)
+{
+	gtk_widget_destroy(debugger->window);
+	object_delete(debugger);
+}
+
+
+/* callbacks */
+/* debugger_on_close */
+static void _debugger_on_close(gpointer data)
+{
+	Debugger * debugger = data;
+
+	/* FIXME really implement */
+	gtk_main_quit();
+}
+
+
+/* debugger_on_closex */
+static gboolean _debugger_on_closex(gpointer data)
+{
+	Debugger * debugger = data;
+
+	_debugger_on_close(debugger);
+	return TRUE;
+}
