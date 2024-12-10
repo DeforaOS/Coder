d455e78f5cce56acfc9d158fd255fb29ec4ab97c
Pierre Pronchery <khorben@defora.org>
2024-12-10 01:12:11 +0100

    Update the copyright notice

--- src/coder.c
+++ src/coder.c
@@ -1,6 +1,6 @@
 /* $Id$ */
 static char const _copyright[] =
-"Copyright © 2011-2020 Pierre Pronchery <khorben@defora.org>";
+"Copyright © 2011-2024 Pierre Pronchery <khorben@defora.org>";
 /* This file is part of DeforaOS Desktop Coder */
 static char const _license[] =
 "This program is free software: you can redistribute it and/or modify\n"
